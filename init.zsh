ede9ba1885902af80c3cdf5355c7986c7489803e
Ota Mares <1375307+omares@users.noreply.github.com>
2023-07-24 18:13:00 +0200

    Add keybinding and update documentation

--- init.zsh
+++ init.zsh
@@ -1,4 +1,26 @@
 if (( $+commands[zoxide] )); then
-    $commands[zoxide] init zsh >| ${0:A:h}/zoxide.zsh
-    source ${0:A:h}/zoxide.zsh
+    $commands[zoxide] init zsh >| ${0:a:h}/zoxide.zsh
+    source ${0:a:h}/zoxide.zsh
+
+    # adds custom CMD+G keybinding to interactively select cd directory
+    # based on https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh#L74-L94
+    _run-cdi() {
+        local dir="$(eval "zoxide query -i")"
+
+        if [[ -z "$dir" ]]; then
+            zle redisplay
+            return 0
+        fi
+
+        zle push-line
+        BUFFER="builtin cd -- ${(q)dir}"
+        zle accept-line
+        local ret=$?
+        unset dir
+        zle reset-prompt
+        return $ret
+    }
+
+    zle -N _run-cdi
+    bindkey "^G" _run-cdi
 fi
